ef047a6dbdfa7cc2295a0c6396829c9fd89130a8
Richa <richasssssssssharma@gmail.com>
2025-10-30 15:08:24 +0530

    second commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
-
+//checking for unstaged changes 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
 	}
