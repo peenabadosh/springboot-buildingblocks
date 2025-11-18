9585bb60660221471bb735bdca6ca1d9c6148a4e
Richa <richasssssssssharma@gmail.com>
2025-11-18 22:57:02 +0530

    actuaor-adminServer-FirstCommit

---
+++ src/main/java/com/stacksimplify/restservices/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.stacksimplify.restservices.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+}
